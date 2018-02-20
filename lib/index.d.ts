71f0de266e0a909c637e2d28bdd786b47f8336d2
slikts <dabas@untu.ms>
2018-02-20 17:29:38 +0200

    Fix missing Cheerio types in TS typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../@types/cheerio/index.d.ts" />
+
 declare namespace scrapeIt {
     export interface ScrapeOptions {
         [key: string]: string | ScrapeOptionList | ScrapeOptionElement;
